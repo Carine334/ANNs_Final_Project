5d084c07ce84e9de1ff61eb9bd687be14947db0d
Carine334 <100688326+Carine334@users.noreply.github.com>
2024-03-22 13:34:24 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, request, Response
-from templates import *
+from flask import Flask, render_template
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
+from skimage import color, exposure, transform
 
-app= Flask(__name__, static_url_path='/static')
-
+app = Flask(__name__, static_url_path='/static')
 
 # Load pre-trained model
-#model = load_model("traffic_signs_model.keras") 
-
+model = load_model("model_aug.h5")
 
 @app.route("/")
 def start():
@@ -20,9 +18,42 @@ def webcam():
     return render_template('webcam.html')
 
 
+#variables
+IMG_SIZE = 48
 
-import traceback
+class UnetInferrer:
+    def __init__(self):
+        self.model = load_model('model_aug.h5')
 
-@app.errorhandler(500)
-def internal_error(exception):
-    return "<pre>"+traceback.format_exc()+"</pre>"
+    def get_prediction(self, image):
+        # Preprocess image
+        image = self.preprocess_image(image)
+        # Perform prediction with the model
+        prediction = self.model.predict(image)
+        return prediction
+
+    def preprocess_image(self, img):
+        # Preprocess image here
+        # For example:
+        # Convert to HSV
+        hsv = color.rgb2hsv(img)
+        # Equalize histogram
+        hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+        img = color.hsv2rgb(hsv)
+        # Resize
+        img = transform.resize(img, (IMG_SIZE, IMG_SIZE), mode='constant')
+        return np.expand_dims(img, axis=0) # Add batch dimension
+
+# Create an instance of UnetInferrer
+m = UnetInferrer()
+
+# Example usage:
+image_data = cv2.imread('photo_5442879035244927474_y.jpg')
+prediction = m.get_prediction(image_data)
+print(prediction)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
