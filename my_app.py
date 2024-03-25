4b0c3dd3ab944798ea9ea2418b2c432a466b6102
Carine334 <100688326+Carine334@users.noreply.github.com>
2024-03-25 11:35:26 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model, Model
+from tensorflow.keras.models import load_model
 import os
 
 app = Flask(__name__, static_url_path='/static')
@@ -71,55 +71,17 @@ def preprocess_images(img):
     img = img.astype("float32") / 255.0
     return img
 
-# Function to generate Grad-CAM visualization
-def generate_grad_cam(model, last_conv_layer_name, image_tensor):
-    # Get the model without the last softmax layer
-    grad_model = Model([model.inputs], [model.get_layer(last_conv_layer_name).output, model.output])
-    
-    # Get the gradient tape
-    with tf.GradientTape() as tape:
-        conv_outputs, predictions = grad_model(image_tensor)
-        class_index = tf.argmax(predictions, axis=1)
-        loss = predictions[:, class_index]
-
-    # Get the gradients of the last conv layer with respect to the loss
-    grads = tape.gradient(loss, conv_outputs)
-
-    # Global average pooling
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    # Multiply each channel in the feature map array by "how important this channel is" with regard to the class
-    heatmap = tf.reduce_mean(tf.multiply(pooled_grads, conv_outputs), axis=-1)
-
-    # Normalize the heatmap
-    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-    
-    return heatmap.numpy()
-
-# Modify the predict_result function to also return Grad-CAM visualization
+# Predicting function
 def predict_result(image):
     pred = model.predict(image)
     predicted_label = np.argmax(pred[0], axis=-1)
     confidence = pred[0][predicted_label]
-    
     if confidence < threshold:
         predicted_class_name = "Not known traffic"
         confidence = 1 - confidence
-        grad_cam = None
     else:
         predicted_class_name = classes[predicted_label]
-        
-        # Generate Grad-CAM
-        last_conv_layer_name = 'dense_3'  # Change this to the name of your last convolutional layer
-        heatmap = generate_grad_cam(model, last_conv_layer_name, image)
-        
-        # Resize heatmap to match the image size
-        heatmap = cv2.resize(heatmap, (image.shape[2], image.shape[1]))
-        heatmap = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
-        grad_cam = cv2.addWeighted(image[0], 0.5, heatmap, 0.5, 0)
-    
-    return predicted_class_name, confidence, grad_cam
-
+    return predicted_class_name, confidence
 
 @app.route("/")
 def start():
@@ -128,6 +90,33 @@ def start():
 @app.route('/webcam.html')
 def webcam():
     return render_template('webcam.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return jsonify(error="No file selected")
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify(error="No file selected")
+
+    # Save the uploaded file to the upload folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(file_path)
+
+    # Read the saved image
+    image = cv2.imread(file_path)
+
+    # Preprocess the image
+    preprocessed_image = preprocess_images(image)
+
+    # Predict the result
+    prediction, confidence = predict_result(np.expand_dims(preprocessed_image, axis=0))
+
+    return jsonify(name_class=prediction, confidence_level = float(confidence))
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 # Update the upload route to include Grad-CAM visualization in the response
